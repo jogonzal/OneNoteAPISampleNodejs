5e29747f75821e929ab41bc621549edbc695adb8
Jorge <jogonzal@microsoft.com>
2016-04-20 16:10:27 -0700

    Increasing version of bootstrap

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    //Live Connect API information
+    // Live Connect API information
     clientId: '000000004011C945',
     clientSecret: 'U5ofKaQiol75VbohV4hh-2x8XZpcJoPf ',
     redirectUrl: 'http://onenoteapisamples.com:3000/callback'
